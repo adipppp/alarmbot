9716233257f8823c258a5c0af27579a451641b75
adipppp <adhif2004@gmail.com>
2026-04-04 18:04:36 +0700

    feat: Enhance Alarm and AlarmRepository with additional properties and methods

--- src/domain/alarm/Alarm.ts
+++ src/domain/alarm/Alarm.ts
@@ -2,6 +2,7 @@ export interface AlarmProps {
     id?: bigint;
     guildId: string;
     channelId: string;
+    timeOfDay: string;
     triggerAt: Date;
     createdAt?: Date;
     isDeleted?: boolean;
@@ -10,16 +11,18 @@ export interface AlarmProps {
 export class Alarm {
     readonly id?: bigint;
     readonly guildId: string;
-    readonly channelId: string;
-    readonly triggerAt: Date;
+    private _channelId: string;
+    private _timeOfDay: string;
+    private _triggerAt: Date;
     readonly createdAt: Date;
     private _isDeleted: boolean;
 
     constructor(props: AlarmProps) {
         this.id = props.id;
         this.guildId = props.guildId;
-        this.channelId = props.channelId;
-        this.triggerAt = props.triggerAt;
+        this._channelId = props.channelId;
+        this._timeOfDay = props.timeOfDay;
+        this._triggerAt = props.triggerAt;
         this.createdAt = props.createdAt ?? new Date();
         this._isDeleted = props.isDeleted ?? false;
     }
@@ -34,8 +37,31 @@ export class Alarm {
         return new Alarm(props);
     }
 
+    reschedule(props: {
+        channelId: string;
+        timeOfDay: string;
+        triggerAt: Date;
+    }): void {
+        this._channelId = props.channelId;
+        this._timeOfDay = props.timeOfDay;
+        this._triggerAt = props.triggerAt;
+        this._isDeleted = false;
+    }
+
     cancel(): void {
         this._isDeleted = true;
+    }
+
+    get channelId(): string {
+        return this._channelId;
+    }
+
+    get timeOfDay(): string {
+        return this._timeOfDay;
+    }
+
+    get triggerAt(): Date {
+        return this._triggerAt;
     }
 
     get isDeleted(): boolean {
--- src/domain/alarm/AlarmRepository.ts
+++ src/domain/alarm/AlarmRepository.ts
@@ -2,6 +2,9 @@ import { Alarm } from "./Alarm";
 
 export interface AlarmRepository {
     findById(id: bigint): Promise<Alarm | null>;
+    findByGuildAndTimeOfDay(guildId: string, timeOfDay: string): Promise<Alarm | null>;
+    findActiveByGuildId(guildId: string): Promise<Alarm[]>;
+    findAllActive(): Promise<Alarm[]>;
     save(alarm: Alarm): Promise<Alarm>;
     delete(id: bigint): Promise<void>;
 }
